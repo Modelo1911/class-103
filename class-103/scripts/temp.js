d66872e4aaf373de8e025ea6c13ee8d8a2a4c456
Guillermo Escobar <155601587+Modelo1911@users.noreply.github.com>
2024-02-02 20:58:05 -0800

    Add files via upload

--- class-103/scripts/temp.js
+++ class-103/scripts/temp.js
@@ -7,21 +7,16 @@ function celToFahr(){
 
 
 function convertTemperature() {
-    // Get user input
     let temperature = parseFloat(document.getElementById('temperature').value);
     let scale = document.getElementById('scale').value;
 
-    // Check the scale and convert temperature accordingly
     if (scale === 'C') {
-      // Convert Celsius to Fahrenheit
       temperature = (temperature * 9/5) + 32;
       document.getElementById('result').innerHTML = `${temperature.toFixed(2)} Fahrenheit`;
     } else if (scale === 'F') {
-      // Convert Fahrenheit to Celsius
       temperature = (temperature - 32) * 5/9;
       document.getElementById('result').innerHTML = `${temperature.toFixed(2)} Celsius`;
     } else {
-      // Invalid scale
       document.getElementById('result').innerHTML = 'Invalid scale. Please select "C" for Celsius or "F" for Fahrenheit.';
     }
   }
